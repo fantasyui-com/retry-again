81dcb60c41cbfc3819d63677d83436aaa4f68a95
fantasyui-com <captain@fantasyui.com>
2018-06-27 09:40:04 -0400

    Add end

--- index.js
+++ index.js
@@ -3,13 +3,15 @@ class Retry {
 
   constructor( program , options ){
     const defaults = {count:5, delay:10, start:true};
-    const { count, delay, start } = Object.assign({},defaults,options)
+    const { count, delay, start, end } = Object.assign({}, defaults, options)
+    this.end = end;
     this.count = count;
     this.delay = delay;
     this.program = program;
     this.tries = 0;
     this.failure = null;
     if(start) this.start();
+
   }
 
   start(){
@@ -29,11 +31,13 @@ class Retry {
         }else{
           // console.log('SOCKET MESSAGE FAILURE #%d: GIVING UP.', this.session, this.tries, error.message);
           this.failure = true;
+          if(end) this.end(this.failure);
         }
       }else{
         // console.log('SENT OK, retries: %d', this.session, this.tries);
         //no error, EXIT;
         this.failure = false;
+        if(end) this.end(this.failure);
       }
 
     }, this.delay);
